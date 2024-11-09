243e9ef191a7e06a98429319f8e3cecc34b16361
Zyper Sploit <zypersploit@gmail.com>
2024-11-09 11:39:33 -0600

    Delete TESTS.py

--- TESTS.py
+++
@@ -1,9 +0,0 @@
-def test_detect_eth0_ip():
-
-  expected_ip = "192.168.1.10"
-  actual_ip = detect_eth0_ip()
-  assert actual_ip == expected_ip, "Failed to detect expected IP address"
-
-
-if __name__ == "__main__":
-  unittest.main()
